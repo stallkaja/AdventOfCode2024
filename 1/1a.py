f00999e9b94d1a646a753abaa559f9dbeeb8599c
stallja <92064776+stallja@users.noreply.github.com>
2024-12-12 06:09:41 -0800

    starting day 1 with boilerplate

---
+++ 1/1a.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def main():
+    '''
+    with open('input.txt') as f:
+        linecount = sum(1 for _ in f)
+    #print(linecount)
+    with open('input.txt') as f:
+        lines = [line.rstrip('\n') for line in f]
+    linelen = len(lines[0])
+    #print(lines)
+    '''
+    test = [[1,2,3],[4,5,6],[7,8,9]]
+    print(test[1][0])
+
+if __name__ == "__main__":
+    main()
