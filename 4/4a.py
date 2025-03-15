ea8c6c5443324acf1ca12a0db59123021787675f
James Stallkamp <jamesstallkamp@gmail.com>
2025-03-15 14:31:58 -0700

    adding day 4 part 1

--- 4/4a.py
+++ 4/4a.py
@@ -1,18 +1,36 @@
 import sys
 
 
+def checkNeighbors(x, y, lines):
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if 0 <= x + i < len(lines) and 0 <= y + j < len(lines[x]) and lines[x + i][y + j] == 'M':
+                if 0 <= x + i + i < len(lines) and 0 <= y + j + j < len(lines[x]) and lines[x + i + i][y + j + j] == 'A':
+                    if 0 <= x + i + i + i < len(lines) and 0 <= y + j + j + j < len(lines[x]) and lines[x + i + i + i][y + j + j + j] == 'S':
+                        print("found an M at:" + str(x + i) + ' ' + str(y + j))
+                        print("found an A at:" + str(x + i + i) + ' ' + str(y + j + j))
+                        print("found an S at:" + str(x + i + i + i) + ' ' + str(y + j + j + j))
+                        print("Found XMAS")
+                        count = count + 1
+    return count
+
+
 def main():
-    '''
     with open('input.txt') as f:
         linecount = sum(1 for _ in f)
-    #print(linecount)
     with open('input.txt') as f:
         lines = [line.rstrip('\n') for line in f]
-    linelen = len(lines[0])
-    #print(lines)
-    '''
-    test = [[1,2,3],[4,5,6],[7,8,9]]
-    print(test[1][0])
+    count = 0
+
+    for i in range(0, len(lines)):
+        for j in range(0, len(lines[i])):
+            if lines[i][j] == 'X':
+                print("found an x at:" + str(i) + ' ' + str(j))
+
+                count = count + checkNeighbors(i, j, lines)
+    print(count)
+
 
 if __name__ == "__main__":
     main()
